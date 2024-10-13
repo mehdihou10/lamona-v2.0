deb86e9018d5c8b100236cd933cdbfb5f554a41b
mehdi hou <mehdi5juillet@gmail.com>
2024-10-13 22:54:39 +0200

    fix

--- utils/send.email.js
+++ utils/send.email.js
@@ -1,30 +1,27 @@
-import nodemailer from 'nodemailer';
-
-export const sendEmail = (html, email, subject) => {
+export const sendEmail = async (html, email, subject) => {
     const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
-            user: process.env.USER_EMAIL,    // Using environment variables for security
-            pass: process.env.USER_PASSWORD
-        }
+            user: process.env.USER_EMAIL,
+            pass: process.env.USER_PASSWORD,
+        },
     });
 
     const mailOptions = {
         from: {
-            name: `Mehdi Hou`, // The sender's name
-            address: process.env.USER_EMAIL,  // The sender's email
+            name: `Mehdi Hou`,
+            address: process.env.USER_EMAIL,
         },
-        replyTo: email,                     // Email of the person who filled out the form
-        to: email,                            // The recipient's email
-        subject: subject,                     // Subject of the email
-        html: html,                           // Message body in HTML format
+        replyTo: email,
+        to: email,
+        subject: subject,
+        html: html,
     };
 
-    transporter.sendMail(mailOptions, (err, success) => {
-        if (err) {
-            console.error('Error sending email:', err);
-        } else {
-            console.log('Email sent successfully:', success);
-        }
-    });
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log('Email sent successfully');
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
 };
